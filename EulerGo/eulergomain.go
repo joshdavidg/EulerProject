ae7ed9d341dbce2b7c9f541788925083c5bb6356
Josh-Windows <joshdavidg.13@gmail.com>
2022-09-20 20:59:29 -0600

    updated print to indicate question #

--- EulerGo/eulergomain.go
+++ EulerGo/eulergomain.go
@@ -8,13 +8,13 @@ import (
 
 func main() {
 	q1 := solutions.SumMultiplesOf3or5()
-	fmt.Println("Sum of multiples of 3 or 5 from 1 to 1000: ", q1)
+	fmt.Println("Q1 - Sum of multiples of 3 or 5 from 1 to 1000: ", q1)
 	q2 := solutions.SumEvenFibNumbers()
-	fmt.Println("Sum of even fibonacci terms less than 4 mill: ", q2)
+	fmt.Println("Q2 - Sum of even fibonacci terms less than 4 mill: ", q2)
 	q3 := solutions.LargestPrimeFactor(600851475143)
-	fmt.Println("The largest prime factor of 600851475143 is: ", q3)
+	fmt.Println("Q3 - The largest prime factor of 600851475143 is: ", q3)
 	q4 := solutions.LargestPalidromeProduct()
-	fmt.Println("The largest palidrome product from two 3 digit numbers is: ", q4)
+	fmt.Println("Q4 - The largest palidrome product from two 3 digit numbers is: ", q4)
 	q5 := solutions.SmallestMultiple()
-	fmt.Println("The smallest number divisible by all numbers 1 -> 20 is: ", q5)
+	fmt.Println("Q5 - The smallest number divisible by all numbers 1 -> 20 is: ", q5)
 }
